a70bc1a93a190eebd7f4c853a5a39781ea504bf3
Steven Barrett <damentz@liquorix.net>
2012-10-13 17:02:32 -0500

    Revert old zen-tune merge to make way for Con's official port of BFS.
    Revert "Merge remote-tracking branch 'github/3.6/zen-tune' into 3.6/master"
    
    This reverts commit 68211281d0ba833673f8f60df615cb947c2cbd78, reversing
    changes made to c3747f209069e196e3c7b089d145d9faf94a8e2f.

--- mm/page-writeback.c
+++ mm/page-writeback.c
@@ -36,7 +36,6 @@
 #include <linux/pagevec.h>
 #include <linux/timer.h>
 #include <trace/events/writeback.h>
-#include <linux/zentune.h>
 
 /*
  * Sleep at most 200ms at a time in balance_dirty_pages().
@@ -67,11 +66,7 @@ static long ratelimit_pages = 32;
 /*
  * Start background writeback (via writeback threads) at this percentage
  */
-#if defined(CONFIG_ZEN_DEFAULT)
 int dirty_background_ratio = 10;
-#elif defined(CONFIG_ZEN_CUSTOM)
-int dirty_background_ratio = dirty_background_ratio_custom;
-#endif
 
 /*
  * dirty_background_bytes starts at 0 (disabled) so that it is a function of
@@ -88,11 +83,7 @@ int vm_highmem_is_dirtyable;
 /*
  * The generator of dirty data starts writeback at this percentage
  */
-#if defined(CONFIG_ZEN_DEFAULT)
 int vm_dirty_ratio = 20;
-#elif defined(CONFIG_ZEN_CUSTOM)
-int vm_dirty_ratio = vm_dirty_ratio_custom;
-#endif
 
 /*
  * vm_dirty_bytes starts at 0 (disabled) so that it is a function of
